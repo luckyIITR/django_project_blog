499e9bf27afb1cc3b7664c5e76120e19da972f72
Lucky Kushwha <luckykushwaha56@gmail.com>
2022-01-14 13:16:12 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.http.response import HttpResponsePermanentRedirect
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse("<h1>Hello world</h1>")
+
+
+def about(request):
+    return HttpResponse("<h1>About Page</h1>")
